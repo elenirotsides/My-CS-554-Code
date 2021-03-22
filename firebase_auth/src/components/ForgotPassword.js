c4eaaf3d7d02ca78cda235079b6e3b1babcf3241
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-03-21 20:08:28 -0400

    rename firebase auth

---
+++ firebase_auth/src/components/ForgotPassword.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function ForgotPassword() {
+    return (
+        <div>
+            <h2>This is the Forgot Password page</h2>
+        </div>
+    );
+}
+
+export default ForgotPassword;
---
+++ firebase_auth/src/components/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function SignIn() {
+    return (
+        <div>
+            <h2>This is the Sign In page</h2>
+        </div>
+    );
+}
+
+export default SignIn;
