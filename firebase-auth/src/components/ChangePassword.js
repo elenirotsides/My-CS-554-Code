c749be7656a3ccc73b5b93c3b0521db0dc6a7979
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-03-21 20:04:09 -0400

    firebase auth

---
+++ firebase-auth/src/components/ChangePassword.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function ChangePassword() {
+    return (
+        <div>
+            <h2>This is the Change Password page</h2>
+        </div>
+    );
+}
+
+export default ChangePassword;
---
+++ firebase-auth/src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function SignUp() {
+    return (
+        <div>
+            <h2>This is the Sign Out page</h2>
+        </div>
+    );
+}
+
+export default SignUp;
