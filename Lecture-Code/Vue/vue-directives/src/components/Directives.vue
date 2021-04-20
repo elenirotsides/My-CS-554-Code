c3d1e3c644ab975d4a6122de5d9dcb28d172d8e0
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-04-20 13:40:10 -0400

    vue directives

---
+++ Lecture-Code/Vue/vue-directives/src/components/Directives.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        {{ msg + ', ' + name }}
+        <br />
+        <span v-once>Span: {{ msg + ', ' + name }}</span>
+        <br />
+        {{ btnDisabled ? 'The button is disabled' : 'The button is enabled' }}
+        <br />
+        <span v-if="btnDisabled">Span: The button is disabled</span>
+        <span v-else>Span: The button is enabled</span>
+        <br />
+        <button v-on:click="changeName" :disabled="btnDisabled">Change Name</button>
+        <br />
+        <ul>
+            <li v-for="(fruit, index) in fruits" :key="index">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Directives',
+    data() {
+        return {
+            name: 'Eleni',
+            btnDisabled: false,
+            fruits: ['Apple', 'Orange', 'Pear'],
+        };
+    },
+    props: {
+        msg: String,
+    },
+    methods: {
+        changeName() {
+            if (this.name === 'Eleni') {
+                this.name = 'John';
+            } else {
+                this.name = 'Eleni';
+            }
+        },
+    },
+};
+</script>
